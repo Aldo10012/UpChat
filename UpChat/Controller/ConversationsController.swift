0b23327cbf127fb66ba27f65de65720a1a520b25
Alberto <aldo10012@gmail.com>
2021-07-16 16:20:49 -0700

    set up home navigation bar

--- UpChat/Controller/ConversationsController.swift
+++ UpChat/Controller/ConversationsController.swift
@@ -29,10 +29,28 @@ class ConversationsController: UIViewController {
     func configueUI() {
         view.backgroundColor = .white
         
+        configureNavigationBar()
+    }
+    
+    func configureNavigationBar() {
+        let appearance = UINavigationBarAppearance()
+        appearance.configureWithOpaqueBackground()
+        appearance.largeTitleTextAttributes = [.foregroundColor: UIColor.white]
+        appearance.backgroundColor = .systemPurple
+        
+        navigationController?.navigationBar.standardAppearance = appearance
+        navigationController?.navigationBar.compactAppearance = appearance
+        navigationController?.navigationBar.scrollEdgeAppearance = appearance
+        
         navigationController?.navigationBar.prefersLargeTitles = true
         navigationItem.title = "Messages"
         let image = UIImage(systemName: "person.circle.fill")
         navigationItem.leftBarButtonItem = UIBarButtonItem(image: image, style: .plain, target: self, action: #selector(showProfile))
+        
+        navigationController?.navigationBar.tintColor = .white
+        navigationController?.navigationBar.isTranslucent = true
+        navigationController?.navigationBar.overrideUserInterfaceStyle = .dark
+        
     }
     
 }
