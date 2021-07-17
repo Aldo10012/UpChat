99a948a50f48234d00a00cd76a956fc34dd8d8ab
Alberto <aldo10012@gmail.com>
2021-07-16 17:54:33 -0700

    setup messages TableView

--- UpChat/Controller/ConversationsController.swift
+++ UpChat/Controller/ConversationsController.swift
@@ -7,9 +7,13 @@
 
 import UIKit
 
+private let reuseIdentifier = "ConversationCell"
+
 class ConversationsController: UIViewController {
     
     // MARK: - Properties
+    
+    private let tableView = UITableView()
     
     // MARK: - Life Cycles
     
@@ -30,6 +34,7 @@ class ConversationsController: UIViewController {
         view.backgroundColor = .white
         
         configureNavigationBar()
+        configureTableView()
     }
     
     func configureNavigationBar() {
@@ -50,7 +55,38 @@ class ConversationsController: UIViewController {
         navigationController?.navigationBar.tintColor = .white
         navigationController?.navigationBar.isTranslucent = true
         navigationController?.navigationBar.overrideUserInterfaceStyle = .dark
-        
     }
+    
+    func configureTableView() {
+        tableView.backgroundColor = .white
+        tableView.rowHeight = 80
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: reuseIdentifier)
+        tableView.tableFooterView = UIView()
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        view.addSubview(tableView)
+        tableView.frame = view.frame
+    }
+    
+}
+
+extension ConversationsController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(indexPath.row)
+    }
+}
+
+extension ConversationsController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath)
+        cell.textLabel?.text = "Test cell"
+        return cell
+    }
+    
     
 }
