4e9108a01171a21ee687547243cb5eca03e89a74
Alberto <aldo10012@gmail.com>
2021-10-22 13:08:37 -0700

    modified message viewModel to change the bubble anchors

--- UpChat/View/MessageCell.swift
+++ UpChat/View/MessageCell.swift
@@ -15,6 +15,9 @@ class MessageCell: UICollectionViewCell {
         didSet { configure() }
     }
     
+    var bubbleLeftAnchor: NSLayoutConstraint!
+    var bubbleRightAnchor: NSLayoutConstraint!
+    
     private let profileImageView: UIImageView = {
         let iv = UIImageView()
         iv.contentMode = .scaleAspectFill
@@ -52,9 +55,14 @@ class MessageCell: UICollectionViewCell {
         
         addSubview(bubbleContainer)
         bubbleContainer.layer.cornerRadius = 12
-        bubbleContainer.anchor(top: topAnchor, left: profileImageView.rightAnchor, paddingLeft: 12)
+        bubbleContainer.anchor(top: topAnchor)
         bubbleContainer.widthAnchor.constraint(lessThanOrEqualToConstant: 250).isActive = true
         bubbleContainer.heightAnchor.constraint(greaterThanOrEqualToConstant: 40).isActive = true
+        
+        bubbleLeftAnchor = bubbleContainer.leftAnchor.constraint(equalTo: profileImageView.rightAnchor, constant: 12)
+        bubbleLeftAnchor.isActive = false
+        bubbleRightAnchor = bubbleContainer.rightAnchor.constraint(equalTo: rightAnchor, constant: -12)
+        bubbleRightAnchor.isActive = false
         
         bubbleContainer.addSubview(textView)
         textView.anchor(top: bubbleContainer.topAnchor, left: bubbleContainer.leftAnchor, bottom: bubbleContainer.bottomAnchor, right: bubbleContainer.rightAnchor, paddingTop: 4, paddingLeft: 12, paddingBottom: 4, paddingRight: 12)
@@ -68,6 +76,11 @@ class MessageCell: UICollectionViewCell {
         bubbleContainer.backgroundColor = viewModel.messageBackgroundColor
         textView.textColor = viewModel.messageTextColor
         textView.text = message.text
+        
+        bubbleLeftAnchor.isActive = viewModel.leftAnchorIsActive
+        bubbleRightAnchor.isActive = viewModel.rightAnchorIsActive
+        
+        profileImageView.isHidden = viewModel.shouldHideProfileImage
     }
     
     required init?(coder: NSCoder) {
--- UpChat/ViewModel/MessageViewModel.swift
+++ UpChat/ViewModel/MessageViewModel.swift
@@ -11,13 +11,31 @@ struct MessageViewModel {
     
     private let message: Message
     
+    /// sets the message background color based on if is current user
     var messageBackgroundColor: UIColor {
         return message.isFromCurrentUser ? #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)  : .systemPurple
     }
     
+    /// sets the message text color based on if is current user
     var messageTextColor: UIColor {
         return message.isFromCurrentUser ? .black : .white
     }
+    
+    /// set message to right if from current user
+    var rightAnchorIsActive: Bool {
+        return message.isFromCurrentUser
+    }
+    
+    /// set message to left if NOT from current user
+    var leftAnchorIsActive: Bool {
+        return !message.isFromCurrentUser
+    }
+    
+    /// if message is from current user, don't show profile image
+    var shouldHideProfileImage: Bool {
+        return message.isFromCurrentUser
+    }
+    
     
     init(message: Message) {
         self.message = message
